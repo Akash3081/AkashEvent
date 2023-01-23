a567ac99f154f62c8be43865ac6a9e5654c1ab63
Akash3081 <akashgupta@blubirch.com>
2023-01-23 10:48:17 +0530

    latest commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'events/index'
+  devise_for :users
   root 'homes#index'
   get '/about', to: 'homes#about'
   get '/contact', to: 'homes#contact'
   resources :events
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
